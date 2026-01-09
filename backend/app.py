5430a5cacc5b046daa469abef1eb3c24cd48a962
Onkar4578 <omkarvirakt11@gmail.com>
2026-01-09 22:14:07 +0530

    Implemented mysql db and created farmer profile API

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,30 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_mysqldb import MySQL
+import config
 
 app = Flask(__name__)
+
+app.config['MYSQL_HOST'] = config.MYSQL_HOST
+app.config['MYSQL_USER'] = config.MYSQL_USER
+app.config['MYSQL_PASSWORD'] = config.MYSQL_PASSWORD
+app.config['MYSQL_DB'] = config.MYSQL_DB
+
+mysql = MySQL(app)
 
 @app.route('/')
 def home():
     return "AgriAssist AI Backend Running Successfully"
+
+@app.route('/add-farmer', methods=['POST'])
+def add_farmer():
+    data = request.json
+    cur = mysql.connection.cursor()
+    cur.execute(
+        "INSERT INTO farmers_profile(name, location, soil_type) VALUES(%s,%s,%s)",
+        (data['name'], data['location'], data['soil_type'])
+    )
+    mysql.connection.commit()
+    return jsonify({"message": "Farmer added successfully"})
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ backend/config.py
@@ -0,0 +1,4 @@
+MYSQL_HOST = 'localhost'
+MYSQL_USER = 'root'
+MYSQL_PASSWORD = 'Onkar@4518'
+MYSQL_DB = 'agriassist'
